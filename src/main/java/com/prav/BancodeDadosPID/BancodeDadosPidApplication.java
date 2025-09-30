2c05072fb473d37c6a55569709b5f538a85097ce
phablocesarb <phablocesar@gmail.com>
2025-09-29 23:16:04 -0300

    Inclusão do Restante dos Arquivos do Projeto

---
+++ src/main/java/com/prav/BancodeDadosPID/BancodeDadosPidApplication.java
@@ -0,0 +1,13 @@
+package com.prav.BancodeDadosPID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodeDadosPidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodeDadosPidApplication.class, args);
+	}
+
+}
